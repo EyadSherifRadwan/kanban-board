b93f4bddd670f046a12c72a2699edeed71673a39
EyadSherifRadwan <eyadut1@gmail.com>
2023-03-12 06:16:15 +0200

    Just a new update to fix some issues and adding Arabic font

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,35 +1,120 @@
 window.addEventListener('load', () => {
+  // saving all existing tasks
   [...allTasks].forEach((task) => {
     const inputField = task.querySelector('input');
     inputField.blur();
     inputField.setAttribute('disabled', '');
     inputField.setAttribute('value', inputField.value);
-    inputField.style.cursor = 'move';
     task.setAttribute('draggable', 'true');
     updateStorage();
   });
 
-  const currentColor = window.localStorage.getItem('currentColor');
-  if (currentColor) {
-    const shadeTwo = window.localStorage.getItem('shadeTwo');
-    const shadeThree = window.localStorage.getItem('shadeThree');
-    const textColor = window.localStorage.getItem('textColor');
+  // adding eventlisteners on the addBtns
+  allAddBtns.forEach((btn) => {
+    btn.addEventListener('click', addTask.bind(null, btn));
+  });
 
+  // adding the color picker
+  const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'nano', // or 'monolith', or 'nano'
+
+    default: `${window.localStorage.getItem('currentColor')}`,
+
+    swatches: [
+      'rgba(244, 67, 54, 1)',
+      'rgba(233, 30, 99, 0.95)',
+      'rgba(156, 39, 176, 0.9)',
+      'rgba(103, 58, 183, 0.85)',
+      'rgba(63, 81, 181, 0.8)',
+      'rgba(33, 150, 243, 0.75)',
+      'rgba(3, 169, 244, 0.7)',
+      'rgba(0, 188, 212, 0.7)',
+      'rgba(0, 150, 136, 0.75)',
+      'rgba(76, 175, 80, 0.8)',
+      'rgba(139, 195, 74, 0.85)',
+      'rgba(205, 220, 57, 0.9)',
+      'rgba(255, 235, 59, 0.95)',
+      'rgba(255, 193, 7, 1)',
+    ],
+
+    components: {
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+        hex: false,
+        rgba: false,
+        hsla: false,
+        hsva: false,
+        cmyk: false,
+        input: true,
+        clear: true,
+        save: false,
+      },
+    },
+
+    i18n: {
+      'btn:clear': 'Reset',
+    },
+  });
+
+  // adding event listeners on the color picker
+
+  // every time you change the color in the color picker this code will fire up
+  pickr.on('change', (...args) => {
+    const currentColor = args[0].toHSLA();
+    const currentColorHEX = args[0].toHEXA();
+
+    // updating all the current color values in the storage in hsla() form
+    window.localStorage.setItem('currentColor', currentColorHEX);
+    window.localStorage.setItem('h', Math.round(currentColor[0]));
+    window.localStorage.setItem('s', `${Math.round(currentColor[1])}%`);
+    window.localStorage.setItem('l', `${Math.round(currentColor[2])}%`);
+    window.localStorage.setItem('a', currentColor[3]);
+
+    if (currentColor[2] > 70) {
+      window.localStorage.setItem('textColor', 'black');
+    } else {
+      window.localStorage.setItem('textColor', '#fff');
+    }
+
+    // updating the current color in the dom so it gets relfected in the css
     document
       .querySelector(':root')
-      .style.setProperty('--mainColor', currentColor);
-    document.querySelector(':root').style.setProperty('--shadeTwo', shadeTwo);
+      .style.setProperty('--h', Math.round(currentColor[0]));
     document
       .querySelector(':root')
-      .style.setProperty('--shadeThree', shadeThree);
-    document.querySelector(':root').style.setProperty('--textColor', textColor);
-  }
-});
+      .style.setProperty('--s', `${Math.round(currentColor[1])}%`);
+    document
+      .querySelector(':root')
+      .style.setProperty('--l', `${Math.round(currentColor[2])}%`);
+    document.querySelector(':root').style.setProperty('--a', currentColor[3]);
+    document
+      .querySelector(':root')
+      .style.setProperty(
+        '--textColor',
+        window.localStorage.getItem('textColor')
+      );
+  });
 
-// ///////////
+  // every time you click the reset button this code will fire up
+  pickr.on('clear', () => {
+    // removing all the currentcColor values from the storage
+    window.localStorage.removeItem('currentColor');
+    window.localStorage.removeItem('h');
+    window.localStorage.removeItem('s');
+    window.localStorage.removeItem('l');
+    window.localStorage.removeItem('a');
 
-// ///////////
-
-allAddBtns.forEach((btn) => {
-  btn.addEventListener('click', addTask.bind(null, btn));
+    // removing all the currentColor values from the dom
+    document.querySelector(':root').style.removeProperty('--h');
+    document.querySelector(':root').style.removeProperty('--s');
+    document.querySelector(':root').style.removeProperty('--l');
+    document.querySelector(':root').style.removeProperty('--a');
+    document.querySelector(':root').style.removeProperty('--textColor');
+  });
 });
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,10 +1,5 @@
-const notStartedTasksList = document.getElementById('not-started-tasks-list');
-const inProgressTasksList = document.getElementById('in-progress-tasks-list');
-const completedTasksList = document.getElementById('in-completed-list');
-
 const allLists = document.querySelectorAll('ul');
 const allTasks = document.querySelectorAll('li:has(input)');
-
 const allAddBtns = [...document.querySelectorAll('li button')];
 
 const addTask = (btn) => {
@@ -12,9 +7,6 @@ const addTask = (btn) => {
   const newTask = document.createElement('li');
   newTask.innerHTML = `
   <input type="text" class="task-input" />
-  <svg class="edit-task-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-  </svg>
   <svg class="delete-task-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
   </svg>
@@ -47,6 +39,7 @@ const showIcons = () => {
 };
 
 const updateStorage = () => {
+  // takes a copy of the html code and stores it the local storage
   window.localStorage.setItem(
     'mainInnerHTML',
     document.querySelector('main').innerHTML
@@ -57,11 +50,10 @@ function addEventListenersOnAllCurrentTasks() {
   const tasks = [...document.querySelectorAll('li:has(input)')];
   tasks.forEach((task) => {
     const inputField = task.querySelector('input');
-    const editBtn = task.querySelector('.edit-task-icon');
     const deleteBtn = task.querySelector('.delete-task-icon');
     const moveBtn = task.querySelector('.up-down-icon');
 
-    // event listeners
+    // handlres
     const saveTask = () => {
       inputField.blur();
       inputField.setAttribute('disabled', '');
@@ -98,19 +90,13 @@ function addEventListenersOnAllCurrentTasks() {
       updateStorage();
     };
 
-    const taskContexMenuHandler = (e) => {
-      e.preventDefault();
-    };
-
+    // event listeners
     inputField.addEventListener('focusout', onFocusOutHandler);
 
+    // when pressing enter
     inputField.addEventListener('keypress', saveTaskHandler);
 
     task.addEventListener('click', editTaskHandler);
-
-    // task.addEventListener('contextmenu', taskContexMenuHandler);
-
-    editBtn.addEventListener('click', editTaskHandler);
 
     deleteBtn.addEventListener('click', deleteTaskHandler);
 
@@ -138,7 +124,6 @@ function addEventListenersOnAllCurrentTasks() {
     // ...............
 
     // drag and drop
-
     const dragStart = () => {
       hideIcons();
       task.classList.add('dragged');
@@ -174,7 +159,9 @@ function addEventListenersOnAllCurrentTasks() {
     list.addEventListener('dragover', (e) => {
       e.preventDefault();
       const target = e.target.closest('li');
-      target.classList.add('target');
+      if (target) {
+        target.classList.add('target');
+      }
     });
 
     list.addEventListener('dragleave', () => {
