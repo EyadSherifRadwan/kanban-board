38dfd48b4bcb98303057ada0380b3cfc8cc139c8
EyadSherifRadwan <124080185+EyadSherifRadwan@users.noreply.github.com>
2023-07-25 01:36:33 +0300

    refactored the code abit and removed some lines of code related to drag and drop on mobile

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,38 +5,10 @@ allAddBtns.forEach((btn) => {
 
 // getting tasks from lcoal storage and rendering it in the dom
 if (localStorage.tasks) {
-  logTasks = JSON.parse(localStorage.tasks);
-  logTasks.forEach((logList, index) => {
-    currentList = allLists[index];
-    logList.forEach((task) => {
-      const textContent = task.textContent;
-
-      domTask = document.createElement("li");
-      domTask.innerHTML = `
-        <input type="text" class="task-input" style="cursor:move" readonly />
-        <svg class="delete-task-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-        </svg>
-        <svg class="up-down-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-        </svg>
-        `;
-      domTask.querySelector("input").value = textContent;
-      domTask.setAttribute("draggable", "true");
-
-      currentList.lastElementChild.insertAdjacentElement(
-        "beforebegin",
-        domTask
-      );
-
-      addEventListeners(domTask);
-    });
-  });
+  updateDomFromStorage();
 }
 
 //
-const langSwitch = document.querySelector("#langSwitch");
-
 langSwitch.addEventListener("change", () => {
   if (langSwitch.checked) {
     // Switch to Arabic
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,16 +1,10 @@
-let currentColor = localStorage.getItem("colors");
-if (currentColor) {
-  currentColor = JSON.parse(currentColor)["currentColor"];
-} else {
-  currentColor = "#009476";
-}
+const langSwitch = document.querySelector("#langSwitch");
 
 const allLists = [...document.querySelectorAll("ul")];
 const allTasks = [...document.querySelectorAll("li:has(input)")];
 const allAddBtns = [...document.querySelectorAll("li button")];
 
 let logTasks = [[], [], []];
-
 function updateStorage() {
   logTasks = [[], [], []];
 
@@ -23,6 +17,36 @@ function updateStorage() {
   });
 
   localStorage.setItem("tasks", JSON.stringify(logTasks));
+}
+
+function updateDomFromStorage() {
+  logTasks = JSON.parse(localStorage.tasks);
+  logTasks.forEach((logList, index) => {
+    currentList = allLists[index];
+    logList.forEach((task) => {
+      const textContent = task.textContent;
+
+      domTask = document.createElement("li");
+      domTask.innerHTML = `
+        <input type="text" class="task-input" style="cursor:move" readonly />
+        <svg class="delete-task-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+        </svg>
+        <svg class="up-down-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+        </svg>
+        `;
+      domTask.querySelector("input").value = textContent;
+      domTask.setAttribute("draggable", "true");
+
+      currentList.lastElementChild.insertAdjacentElement(
+        "beforebegin",
+        domTask
+      );
+
+      addEventListeners(domTask);
+    });
+  });
 }
 
 const addTask = (btn) => {
@@ -146,43 +170,6 @@ function addEventListeners(task) {
       list
         .querySelectorAll("li:has(input):not(.dragged)")
         .forEach((task) => {});
-    });
-  };
-
-  const touchStartHandler = (e) => {
-    const targetTask = e.target.closest("li");
-    if (targetTask) {
-      targetTask.classList.add("target");
-    }
-  };
-
-  const touchEndHandler = (e) => {
-    const targetTask = e.target.closest("li:has(input)");
-    const targetButton = e.target.closest("li:has(button)");
-
-    // const targetTask = document.querySelector('.target');
-    const dragged = document.querySelector(".dragged");
-    if (dragged && targetTask) {
-      targetTask.insertAdjacentElement("afterend", dragged);
-      targetTask.classList.remove("target");
-      dragged.classList.remove("dragged");
-    } else if (dragged && targetButton) {
-      targetButton.insertAdjacentElement("beforebegin", dragged);
-      targetButton.classList.remove("target");
-      dragged.classList.remove("dragged");
-    } else if (dragged) {
-      dragged.classList.remove("dragged");
-    }
-
-    showIcons();
-
-    document.removeEventListener("touchstart", touchStartHandler);
-    document.removeEventListener("touchend", touchEndHandler);
-
-    allLists.forEach((list) => {
-      list.querySelectorAll("li input").forEach((input) => {
-        input.removeAttribute("disabled");
-      });
     });
   };
 
